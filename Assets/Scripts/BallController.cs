a73e55f51bfd32abf3ef970831edb44c60d01078
MichalSerdynski <michalserdynski@icloud.com>
2023-01-21 14:17:25 -0500

    Paddles Speed
    Decreased paddles speed

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -2,19 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallController : MonoBehaviour {
-    
+public class BallController : MonoBehaviour
+{
+
     public float speed = 10;
     private Rigidbody body;
 
-    void Start() {
+    void Start()
+    {
         body = GetComponent<Rigidbody>();
         Launch();
     }
 
-    void Launch() {
-        float x = Random.Range(0,2) == 0 ? -1 : 1;
-        float z = Random.Range(0,2) == 0 ? -1 : 1;
+    
+    void Launch()
+    {
+        float x = Random.Range(0, 2) == 0 ? -1 : 1;
+        float z = Random.Range(0, 2) == 0 ? -1 : 1;
         body.velocity = new Vector3(speed * x, 0, speed * z);
     }
 }
+
