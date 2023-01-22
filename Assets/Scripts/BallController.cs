9c4c8b3ce1d24af49674b3895d7a5d729554ddc7
Skylar Masson <32114503+Smasson76@users.noreply.github.com>
2023-01-22 00:13:36 -0600

    Changed box collider size on walls

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -2,29 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallController : MonoBehaviour
-{
+public class BallController : MonoBehaviour {
+
+
     public float speed = 10f; // public variable to set the speed of the ball
 
     private Rigidbody rb; // reference to the Rigidbody component of the ball
 
     public float bounciness = 1f; // reference to the Rigidbody component
 
-    void Start()
-    {
+    void Start() {
         // Get the Rigidbody component
         rb = GetComponent<Rigidbody>();
         // Set the ball's velocity to a random vector inside the unit sphere, multiplied by the speed
         rb.velocity = Random.insideUnitSphere * speed;
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
+    private void OnCollisionEnter(Collision collision) {
         // Get the normal of the collision surface
         Vector3 normal = collision.contacts[0].normal;
 
         // Reflect the ball's velocity off the collision surface
         rb.velocity = Vector3.Reflect(rb.velocity, normal) * bounciness;
+
+        Debug.Log("collision");
 
     }
 
