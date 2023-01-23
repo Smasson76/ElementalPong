8c47dd60a0ff97915fba7f81038b28b3c4c98bf5
Skylar Masson <32114503+Smasson76@users.noreply.github.com>
2023-01-23 00:08:12 -0600

    Added scoring system

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour {
 
+     public static BallController instance;     //Creating a singleton of this object
 
     public float speed = 10f; // public variable to set the speed of the ball
 
@@ -11,27 +12,55 @@ public class BallController : MonoBehaviour {
 
     public float bounciness = 1f; // reference to the Rigidbody component
 
-    void Start() {
+    void Awake() {
+        if (instance == null) {
+			instance = this;
+            DontDestroyOnLoad(gameObject);
+		}
+		else {
+			Destroy(gameObject);
+		}
+
         // Get the Rigidbody component
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>(); 
+    }
+
+    public void ResetBallRB() {
         // Set the ball's velocity to a random vector inside the unit sphere, multiplied by the speed
         rb.velocity = Random.insideUnitSphere * speed;
     }
 
     void Update() {
-        Vector3 v = rb.velocity;
-        if (v.magnitude < 0.1f) {
-            v = Vector3.up * speed;
+        Debug.Log("Update");
+        if (GameManager.instance.gameStarted == true) {
+            Debug.Log("game started = true");
+            Vector3 v = rb.velocity;
+            if (v.magnitude < 0.1f) {
+                v = Vector3.up * speed;
+            }
+            if (Mathf.Abs(Vector3.Dot(v.normalized, Vector3.right)) > 0.9f) {
+                v.y *= 1.1f;
+            }
+            rb.velocity = v.normalized * speed;
         }
-        if (Mathf.Abs(Vector3.Dot(v.normalized, Vector3.right)) > 0.9f) {
-            v.y *= 1.1f;
+        else {
+            rb.velocity = Vector3.zero;
+            Debug.Log("game started = false");
         }
-        rb.velocity = v.normalized * speed;
     }
 
     private void OnCollisionEnter(Collision collision) {
         if (collision.contacts.Length > 0) {
             transform.up = collision.contacts[0].normal;
+        }
+    }
+
+    void OnTriggerEnter(Collider c) {
+        if (c.gameObject.CompareTag("LeftBorder")) {
+            GameManager.instance.Player1Scored();
+        }
+        if (c.gameObject.CompareTag("RightBorder")) {
+            GameManager.instance.Player2Scored();
         }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
     
     public static GameManager instance;     //Creating a singleton of this object
 
-    public GameObject playerPrefab;
+    [SerializeField]
+    public int player1Score, player2Score;
+
+    public Text player1ScoreText, player2ScoreText;
+
+    public bool gameStarted;
 
     void Awake() {
         //If this Gamemanager does not exist, set this instance and don't destroy, else destroy
@@ -18,6 +24,36 @@ public class GameManager : MonoBehaviour {
 		else {
 			Destroy(gameObject);
 		}
+
+        player1Score = 0;
+        player2Score = 0;
+        player1ScoreText.text = " " + player1Score;
+        player2ScoreText.text = " " + player2Score;
+        gameStarted = false;
     }
 
+    void Start() {
+        StartCoroutine(StartBall());
+    }
+
+
+    public void Player1Scored() {
+        player1Score++;
+        player1ScoreText.text = " " + player1Score;
+        StartCoroutine(StartBall());
+    }
+
+    public void Player2Scored() {
+        player2Score++;
+        player2ScoreText.text = " " + player2Score;
+        StartCoroutine(StartBall());
+    }
+
+    public IEnumerator StartBall() {
+        gameStarted = false;
+        BallController.instance.transform.position = new Vector3(0, 0.3114f, 0);
+        yield return new WaitForSeconds(4f);
+        BallController.instance.ResetBallRB();
+        gameStarted = true;
+    }
 }
